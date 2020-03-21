971e5d69c40486922ea1447a73b842627b8b7952
Caio Guedes <caio.cesar.g.souza@gmail.com>
2020-03-21 19:45:30 -0300

    add example with test source stream

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,19 @@
-public class App {
-    public String getGreeting() {
-        return "Hello world.";
-    }
+import com.hazelcast.jet.Jet;
+import com.hazelcast.jet.JetInstance;
+import com.hazelcast.jet.pipeline.Pipeline;
+import com.hazelcast.jet.pipeline.Sinks;
+import com.hazelcast.jet.pipeline.test.TestSources;
 
+public class App {
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        Pipeline pipeline = Pipeline.create();
+        pipeline.readFrom(TestSources.itemStream(10))
+            .withoutTimestamps()
+            .filter(event -> event.sequence() % 2 == 0)
+            .setName("filter out odd numbers")
+            .writeTo(Sinks.logger());
+
+        JetInstance jet = Jet.newJetInstance();
+        jet.newJob(pipeline).join();
     }
 }
